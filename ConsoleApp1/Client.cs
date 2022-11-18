5107fac928a507e875f3d8e024a085f15c64498e
Ezgi Cinan <ezgi.cinan@marun.edu.tr>
2022-11-18 13:33:23 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.IO; //StreamReader ve StreamWriter siniflari için
+using System.Linq;
+
+public class Client
+{
+
+    const int PORT_NO = 5000;
+    const string SERVER_IP = "192.168.1.21"; //127.0.0.1
+
+    public static void Main(string[] args) {
+
+        try 
+        {            
+            //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+            Console.WriteLine("Connecting.....");
+            
+            //---data to send to the server---               
+            Console.Write("Enter the string to be transmitted, write '<EOF>' at the en of your message and enter: ");
+            String str = Console.ReadLine();
+            NetworkStream nwStream = client.GetStream();
+            
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(str);
+            
+            Console.WriteLine("Transmitting.....");
+            
+            //---send the text to the connected TcpServer---
+            Console.WriteLine("Sending from Client to the Server: " + str);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            
+            //---read back the text---
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            Console.WriteLine("Received from Server: " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            Console.ReadLine();         
+            
+            //Close
+            nwStream.Close();
+            client.Close();
+        }
+        catch (Exception e){
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }
+
+        Console.WriteLine("\n Press Enter to continue...");
+        Console.ReadLine();
+
+    }
+}
---
+++ ConsoleApp1/Server.cs
@@ -0,0 +1,81 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO; //StreamReader ve StreamWriter siniflari için
+using System.Net.Sockets; // Socket, TcpListener ve NetworkStrem siniflari için
+using System.Net;
+using System.Net.Http;
+
+public class Server
+{
+    const int PORT_NO = 5000; //Set the TcpListener on port 5000.
+    const string SERVER_IP = "192.168.1.21"; //127.0.0.1
+
+    public static void Main(string [] args)
+    {
+        TcpListener listener = null;
+
+        try
+        {
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            listener = new TcpListener(localAdd, PORT_NO);
+
+            //Start listening for client requests.
+            Console.WriteLine("Listening...");
+            listener.Start();
+
+            Console.WriteLine("The server is running at port 5000...");
+            StringBuilder myCompleteMessage = new StringBuilder();
+            
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+                          
+            Console.WriteLine("Connected!");
+            
+            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            string dataReceived = null;
+            int bytesRead = 0;
+            
+            //Take the current time info
+            DateTime now = DateTime.Now;
+            
+            while (true)
+            {
+                //---read incoming stream---
+                bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+                dataReceived += Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            
+                if (dataReceived.IndexOf("<EOF>") > -1)
+                    break;
+            }
+            
+            Console.WriteLine("Received from Client: " + dataReceived);
+            dataReceived += "  " + now.ToString();
+            
+            byte[] byToSend = ASCIIEncoding.ASCII.GetBytes(dataReceived);
+            
+            //---write back the text to the client---
+            Console.WriteLine("Sending back : " + dataReceived);
+            nwStream.Write(byToSend, 0, byToSend.Length);
+            
+            //Shutdown and end connection
+            client.GetStream().Close();
+            client.Close();
+            
+            // Stop listening for new clients.
+            listener.Stop();
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+
+        Console.WriteLine("\nHit enter to continue...");     
+        Console.ReadLine();
+    }
+}
